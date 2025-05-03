b55ceba8e680da00f9d2d534649ceb03e0d1804f
gkfat <gkgkdesign@gmail.com>
2025-05-03 16:57:34 +0800

    feat(client): 已啟用的服務卡片

--- client/src/views/dashboard/Dashboard.vue
+++ client/src/views/dashboard/Dashboard.vue
@@ -6,6 +6,12 @@
         <PageHeader
             :title="t('dashboard.title')"
         />
+
+        <!-- <v-row class="pb-10">
+            <v-col cols="auto">
+                <ActivatedAppsCard />
+            </v-col>
+        </v-row> -->
     </PageContent>
 </template>
 <script lang="ts" setup>
@@ -15,9 +21,11 @@ import { useI18n } from 'vue-i18n';
 
 import PageContent from '@/layouts/panel/PageContent.vue';
 import PageHeader from '@/layouts/panel/PageHeader.vue';
-import { timeFormat } from '@/utils/time';
+
+// import ActivatedAppsCard from './components/ActivatedAppsCard.vue';
 
 const inProgress = ref(false);
 
 const { t } = useI18n();
+
 </script>
---
+++ client/src/views/dashboard/components/ActivatedAppsCard.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-card
+        elevation="3"
+        rounded="lg"
+    >
+        <v-card-title>
+            已啟用的服務
+        </v-card-title>
+        <v-divider />
+        <v-card-text>
+            <v-row class="ma-0 flex-wrap">
+                <v-col
+                    v-for="(item, i) in applications"
+                    :key="i"
+                    cols="12"
+                >
+                    <v-switch
+                        :model-value="item.activated"
+                        readonly
+                        hide-details
+                        density="compact"
+                        :color="item.activated ? 'success' : ''"
+                    >
+                        <template #label>
+                            <p class="pl-3 text-h6">
+                                {{ t('applications.' + item.key) }}
+                            </p>
+                        </template>
+                    </v-switch>
+                </v-col>
+            </v-row>
+        </v-card-text>
+    </v-card>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+const applications = ref([
+    {
+        key: 'portfolio', activated: true, 
+    }, {
+        key: 'popcron', activated: false, 
+    },
+]);
+</script>
